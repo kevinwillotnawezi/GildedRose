1ab842a9cec71cb5ee3ed4059b35d7e95f642068
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-12-23 10:50:19 +0100

    create test when quality decrease

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void qualityShouldDecreaseWithTime() {
+        // Given
+        Item[] items = new Item[] { new Item("foo", 5, 10) };
         GildedRose app = new GildedRose(items);
+
+        // When
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        // Then
+        assertEquals(9, app.items[0].quality);
     }
 
 }
