527345df7ca0dc98f9a7c60f9491491118f8c556
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-12-23 11:11:58 +0100

    add test when sellIn is past and add test template

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,14 +10,51 @@ class GildedRoseTest {
     @Test
     void qualityShouldDecreaseWithTime() {
         // Given
-        Item[] items = new Item[] { new Item("foo", 5, 10) };
+        String name = "test";
+        int sellIn = 5;
+        int quality = 10;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
 
         // When
         app.updateQuality();
 
         // Then
-        assertEquals(9, app.items[0].quality);
+        assertEquals(quality-1, app.items[0].quality);
+        assertEquals( sellIn-1, app.items[0].sellIn);
     }
 
+    @Test
+    void qualityShouldDecreaseFasterAfterExpirationDate() {
+        // Given
+        String name = "test";
+        int sellIn = 0;
+        int quality = 10;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(quality-2, app.items[0].quality);
+        assertEquals( sellIn-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void template() {
+        // Given
+        String name = "test";
+        int sellIn = 0;
+        int quality = 10;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(sellIn, app.items[0].quality);
+        assertEquals(quality, app.items[0].sellIn);
+    }
 }
