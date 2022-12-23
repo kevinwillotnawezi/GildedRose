ff46f9252739421b47377ccc1710a3b627c78c74
Kevin Willot <kevin.willot.nawezi@gmail.com>
2022-12-23 21:55:24 +0100

    create all tests before adding 'Conjured' element

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
@@ -42,11 +43,11 @@ class GildedRoseTest {
     }
 
     @Test
-    void template() {
+    void qualityShouldNotBeNegative() {
         // Given
         String name = "test";
         int sellIn = 0;
-        int quality = 10;
+        int quality = 0;
         Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
 
@@ -54,7 +55,126 @@ class GildedRoseTest {
         app.updateQuality();
 
         // Then
-        assertEquals(sellIn, app.items[0].quality);
-        assertEquals(quality, app.items[0].sellIn);
+        assertEquals(sellIn-1, app.items[0].sellIn);
+        assertTrue(app.items[0].quality >= 0);
+    }
+
+    @Test
+    void agedBrieQualityShouldIncreaseWithTime() {
+        // Given
+        String name = "Aged Brie";
+        int sellIn = 0;
+        int quality = 0;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(sellIn-1, app.items[0].sellIn);
+        assertTrue(quality < app.items[0].quality);
+    }
+
+    @Test
+    void sulfurasShouldNotLoseQuality () {
+        // Given
+        String name = "Sulfuras, Hand of Ragnaros";
+        int sellIn = 0;
+        int quality = 50;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(sellIn, app.items[0].sellIn);
+        assertEquals(quality, app.items[0].quality);
+    }
+
+    @Test
+    void qualityShouldNotBeBiggerThan50() {
+        // Given
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellIn = 11;
+        int quality = 50;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(sellIn-1, app.items[0].sellIn);
+        assertEquals(quality, app.items[0].quality);
+    }
+
+    @Test
+    void backStagePassesQualityShouldIncreaseWithTime() {
+        // Given
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellIn = 11;
+        int quality = 20;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(sellIn-1, app.items[0].sellIn);
+        assertEquals(quality+1, app.items[0].quality);
+    }
+
+    @Test
+    void backStagePassesQualityShouldIncreaseMore10daysBeforeConcert() {
+        // Given
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellIn = 10;
+        int quality = 20;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(sellIn-1, app.items[0].sellIn);
+        assertEquals(quality+2, app.items[0].quality);
+    }
+
+    @Test
+    void backStagePassesQualityShouldIncreaseMore5daysBeforeConcert() {
+        // Given
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellIn = 5;
+        int quality = 20;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(sellIn-1, app.items[0].sellIn);
+        assertEquals(quality+3, app.items[0].quality);
+    }
+
+    @Test
+    void backStagePassesQualityShouldBe0afterConcert() {
+        // Given
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int sellIn = 0;
+        int quality = 20;
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(sellIn-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
     }
 }
